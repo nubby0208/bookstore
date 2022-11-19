5c844c83d174c4a59fc180b88b423446957a8ead
gruTonubby <109526476+gruTonubby@users.noreply.github.com>
2022-11-20 05:45:59 +0900

    update: asset url

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'https://localhost/bookstore/index.php'),
 
-    'asset_url' => env('ASSET_URL', null),
+    'asset_url' => env('ASSET_URL', 'https://localhost/bookstore/'),
 
     /*
     |--------------------------------------------------------------------------

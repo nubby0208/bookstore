31fcf0a5577e903c55f83ab74f308234a2d08783
gruTonubby <109526476+gruTonubby@users.noreply.github.com>
2022-11-20 10:10:32 +0900

    feat: language change by url

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@
 /*Route::get('/', function () {
     return view('welcome');
 });*/
+Route::get('language/{locale}', function ($locale) {
+    app()->setLocale($locale);
+    session()->put('locale', $locale);
+    return redirect()->back();
+});
 
 Route::get('/', 'BookshopHomeController@index')->name('bookshop.home');
 

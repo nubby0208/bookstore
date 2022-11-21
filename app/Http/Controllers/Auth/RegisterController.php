c474984714b68da280fd21b563ee6f4f3bd7288b
gruTonubby <109526476+gruTonubby@users.noreply.github.com>
2022-11-22 07:00:24 +0900

    feat: user register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,10 +66,19 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $result = Whmcs::AddClient([
+            'firstname' => $data['name'],
+            // 'lastname' => $data['name'],
+            'country' => 'us',
+            'email' => $data['email'],
+            'password2' => Hash::make($data['password']),
+        ]);
+        
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'id' => $result['clientid'],
         ]);
     }
 

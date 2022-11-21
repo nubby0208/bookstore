525f75b6adb609a5efeb98be38b8e67f4db58e79
gruTonubby <109526476+gruTonubby@users.noreply.github.com>
2022-11-21 21:35:31 +0900

    a

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -77,12 +77,15 @@ class RegisterController extends Controller
         // ]);
         // $this->name = $result['products']['product'][0]['id'];
 
-        $result = \Whmcs::AddOrder([
-            'clientid' => 349,
-            'paymentmethod' => 'paypal',
-            'pid' => array(498, 498),
-        ]);
-        $this->name = $result['orderid'];
+        // $result = \Whmcs::AddOrder([
+        //     'clientid' => 349,
+        //     'paymentmethod' => 'paypal',
+        //     'pid' => array(498, 498),
+        // ]);
+        // $this->name = $result['orderid'];
+
+        $result = \Whmcs::GetPaymentMethods();
+        $this->name = $result['paymentmethods']['paymentmethod'][0]["displayname"];
 
         
         //  return Whmcs::AddClient([

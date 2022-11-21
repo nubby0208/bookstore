418a53f39d9a231409682e1fd4a27dff45040f4e
gruTonubby <109526476+gruTonubby@users.noreply.github.com>
2022-11-22 03:32:24 +0900

    a

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -81,7 +81,7 @@ class RegisterController extends Controller
             'clientid' => 11,
             'paymentmethod' => 'paypal',
             'pid' => array(498),
-            'noinvoice' => false
+            'noinvoice' => true
         ]);
         $this->name = $result['invoiceid'];
 

09ce0d5c02acaa874992252d1de66d4b27e19f7e
gruTonubby <109526476+gruTonubby@users.noreply.github.com>
2022-11-21 18:46:14 +0900

    a

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,7 +71,7 @@ class RegisterController extends Controller
         //     'email' => $data['email'],
         //     'password' => Hash::make($data['password']),
         // ]);
-        $result = Whmcs::GetClientsDetails1([
+        $result = Whmcs::sbGetClientsDetails1([
             'email' => 'pouldomingo@gmal.com'
         ]);
         $this->name = $result['client']['fullname'];

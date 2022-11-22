b2df1ca49fbd64beedf77e24c668f9678886813f
gruTonubby <109526476+gruTonubby@users.noreply.github.com>
2022-11-23 01:55:56 +0900

    a

--- app/Http/Controllers/Admin/AdminBooksController.php
+++ app/Http/Controllers/Admin/AdminBooksController.php
@@ -35,12 +35,8 @@ class AdminBooksController extends AdminBaseController
             // 'name' => $request->name,
             // 'gid' => 4,
             'type' => 'other',
-            'gid' => '1',
+            'gid' => 1,
             'name' => 'Sample Product111111',
-            'welcomeemail' => '5',
-            'paytype' => 'recurring',
-            'pricing' => array(1 => array('monthly' => 1.00, 'msetupfee' => 1.99, 'quarterly' => 2.00, 'qsetupfee' => 1.99, 'semiannually' => 3.00, 'ssetupfee' => 1.99, 'annually' => 4.00, 'asetupfee' => 1.99, 'biennially' => 5.00, 'bsetupfee' => 1.99, 'triennially' => 6.00, 'tsetupfee' => 1.99)),
-            'recommendations' => array(array('id' => 1, 'order' => 0), array('id' => 2, 'order' => 1)),
         ]);
         if($result["result"] != "success")
             return redirect('/admin/books')

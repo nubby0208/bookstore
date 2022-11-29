ba79ff5b9f9e78d78673a0edec26dfd41ab5ce41
gruTonubby <109526476+gruTonubby@users.noreply.github.com>
2022-11-29 11:42:43 +0900

    fix time credit

--- resources/views/public/book-details.blade.php
+++ resources/views/public/book-details.blade.php
@@ -82,7 +82,7 @@
                                         <input type="hidden" value="{{Auth::user()->id}}" id="user_id">
                                         <input type="hidden" value="{{$book->id}}" id="book_id">
                                         @if(count($book_readstate) == 0)
-                                        <button id="purchase_bt" class="btn btn-success btn-sm" onclick="buy_by_time()">{{ __('Read this book') }}</button>
+                                        <button id="purchase_bt" class="btn btn-success btn-sm" onclick="select_purchasing_method()">{{ __('Read this book') }}</button>
                                         <div class="row" id="hidden_purchasing_method">
                                             <button id="buy_directly" class="btn btn-success btn-sm" onclick="buy_directly()">{{ __('Read directly') }}</button>
                                             <button id="buy_by_time" class="btn btn-success btn-sm" onclick="buy_by_time()">{{ __('Read by time') }}</button>
@@ -127,7 +127,7 @@
                             </div>
                             </div>
                                         @else
-                                        <button id="purchase_bt" class="btn btn-success btn-sm" onclick="buy_by_time()">Read this book</button>
+                                        <button id="purchase_bt" class="btn btn-success btn-sm" onclick="select_purchasing_method()">Read this book</button>
                                         <div class="row" id="hidden_purchasing_method">
                                             <button id="buy_directly" class="btn btn-success btn-sm" onclick="buy_directly()">Read directly</button>
                                             <button id="buy_by_time" class="btn btn-success btn-sm" onclick="buy_by_time()">Read by time</button>
@@ -189,7 +189,7 @@
                             <div id="time_limit">Permission closes in <span id="time">00:00</span> minutes!</div>
                         </div>
                                         @else
-                                        <button id="purchase_bt" class="btn btn-success btn-sm" onclick="buy_by_time()">Read this book</button>
+                                        <button id="purchase_bt" class="btn btn-success btn-sm" onclick="select_purchasing_method()">Read this book</button>
                                         <div class="row" id="hidden_purchasing_method">
                                             <button id="buy_directly" class="btn btn-success btn-sm" onclick="buy_directly()">Read directly</button>
                                             <button id="buy_by_time" class="btn btn-success btn-sm" onclick="buy_by_time()">Read by time</button>

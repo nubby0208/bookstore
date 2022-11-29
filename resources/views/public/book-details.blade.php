46d4fcfac56fb8da548795c49177cb1e0996daa6
gruTonubby <109526476+gruTonubby@users.noreply.github.com>
2022-11-29 11:38:54 +0900

    fix time credit

--- resources/views/public/book-details.blade.php
+++ resources/views/public/book-details.blade.php
@@ -168,9 +168,38 @@
                             </div>
                             <div id="time_limit">Permission closes in <span id="time">{{$book_readstate[0]->remain_min}}:{{$book_readstate[0]->remain_sec}}</span> minutes!</div>
                         </div>
-                                        @else
+                                        
+                                        @elseif($book_readstate[0]->state == 4)
                                         <div class="row" id="hidden_starting_time_bt">
                                             <button id="duration_apply" class="btn btn-danger btn-sm" onclick="select_duration({{$book_readstate[0]->limit_time}})">Apply</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="book-description p-3" id="book_description">
+                                        <p>{!! Markdown::convertToHtml(e($book->description)) !!}</p>
+                                    </div>
+                                </div>
+                                <div class="row justify-content-center mt-4" id="pdf_file">
+                                    <iframe id="pdf_viewer" src="{{asset($pdf_file_url->pdf_file)}}#toolbar=0" width="98%" height="600" allowfullscreen="true">
+                                            This browser does not support PDFs. Please download the PDF to view it: <a href="{{asset($pdf_file_url->pdf_file)}}">Download PDF</a>
+                                    </iframe>
+                                </div>
+                            </div>
+                            <div id="time_limit">Permission closes in <span id="time">00:00</span> minutes!</div>
+                        </div>
+                                        @else
+                                        <button id="purchase_bt" class="btn btn-success btn-sm" onclick="buy_by_time()">Read this book</button>
+                                        <div class="row" id="hidden_purchasing_method">
+                                            <button id="buy_directly" class="btn btn-success btn-sm" onclick="buy_directly()">Read directly</button>
+                                            <button id="buy_by_time" class="btn btn-success btn-sm" onclick="buy_by_time()">Read by time</button>
+                                        </div>
+                                        <div class="row" id="hidden_down_bt">
+                                            <a href="{{asset($pdf_file_url->pdf_file)}}" class="btn btn-success btn-sm" download style="max-width: 130px; width: 100%"><i class="fas fa-download"></i></a>
+                                        </div>
+                                        <div class="row" id="hidden_starting_time_bt">
+                                            <input type="number" value="1" id="duration_time"> minutes
+                                            <button id="duration_apply" class="btn btn-danger btn-sm" onclick="select_duration()">Apply</button>
                                         </div>
                                     </div>
                                 </div>

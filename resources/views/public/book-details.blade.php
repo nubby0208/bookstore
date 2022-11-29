78c649f2839d9f5270044dad4af0a9440468fea4
gruTonubby <109526476+gruTonubby@users.noreply.github.com>
2022-11-29 11:50:23 +0900

    restore

--- resources/views/public/book-details.blade.php
+++ resources/views/public/book-details.blade.php
@@ -169,25 +169,7 @@
                             <div id="time_limit">Permission closes in <span id="time">{{$book_readstate[0]->remain_min}}:{{$book_readstate[0]->remain_sec}}</span> minutes!</div>
                         </div>
                                         
-                                        @elseif($book_readstate[0]->state == 4)
-                                        <div class="row" id="hidden_starting_time_bt">
-                                            <button id="duration_apply" class="btn btn-danger btn-sm" onclick="select_duration({{$book_readstate[0]->limit_time}})">Apply</button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="book-description p-3" id="book_description">
-                                        <p>{!! Markdown::convertToHtml(e($book->description)) !!}</p>
-                                    </div>
-                                </div>
-                                <div class="row justify-content-center mt-4" id="pdf_file">
-                                    <iframe id="pdf_viewer" src="{{asset($pdf_file_url->pdf_file)}}#toolbar=0" width="98%" height="600" allowfullscreen="true">
-                                            This browser does not support PDFs. Please download the PDF to view it: <a href="{{asset($pdf_file_url->pdf_file)}}">Download PDF</a>
-                                    </iframe>
-                                </div>
-                            </div>
-                            <div id="time_limit">Permission closes in <span id="time">00:00</span> minutes!</div>
-                        </div>
+                                        
                                         @else
                                         <button id="purchase_bt" class="btn btn-success btn-sm" onclick="select_purchasing_method()">Read this book</button>
                                         <div class="row" id="hidden_purchasing_method">

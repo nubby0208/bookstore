3f2f3e73a6daea6350ea397bc3558ec435e6ed2f
gruTonubby <109526476+gruTonubby@users.noreply.github.com>
2022-12-15 19:19:16 +0900

    test

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,6 +73,7 @@ class RegisterController extends Controller
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'whmcs' => ['required']
         ], [
+            'name.required' => ['aaaaaaaaa'],
             'whmcs.required' => 'User exist in whmcs, please try another one',
         ]);
     }

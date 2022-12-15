88b41146d8a4b9e5582274d83d2268faddfa9b15
gruTonubby <109526476+gruTonubby@users.noreply.github.com>
2022-12-15 20:14:09 +0900

    test

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,7 +68,7 @@ class RegisterController extends Controller
 
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:50', 'unique:users'],
+            'email' => ['max:50', 'required', 'string', 'email', 'unique:users'],
             'whmcs' => ['required', 'min:8'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ],[

d62fc2346f4013e4762522618a9e492175c070f1
nubby0208 <109526476+gruTonubby@users.noreply.github.com>
2022-12-21 00:20:04 +0900

    pack6

--- app/Http/Controllers/Admin/AdminBooksController.php
+++ app/Http/Controllers/Admin/AdminBooksController.php
@@ -785,7 +785,7 @@ class AdminBooksController extends AdminBaseController
         $final_price  = $request->init_price - $count_discount;
         $input['price'] = $final_price;
 
-        $booknames = $this->pack5booknames;
+        $booknames = $this->pack6booknames;
 
         foreach($booknames as $bookname){
             $input['title'] = $bookname;
